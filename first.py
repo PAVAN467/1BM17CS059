ccddbc3d67137fa677b2242a60060690628a607b
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2019-08-06 15:41:35 +0530

    FIRST

---
+++ first.py
@@ -0,0 +1,35 @@
+#num=int(input("enter the number"))
+#if(num>1):
+ #   for i in range(2,num):
+  #      if(num%i==0):
+   #         print("not a prime")
+#else:
+ #   print("prime")
+
+
+
+
+
+#a=input("enter the a")
+#b=input("enter b")
+#if(a>b):
+ #   print("a is greater")
+#lse:
+#    print("b is greater")
+
+
+a=[10,20,30]
+b=0
+while b!=-1:
+    b=input("enter the code")
+    a.append(b)
+   
+       
+        
+    
+
+
+
+
+
+
