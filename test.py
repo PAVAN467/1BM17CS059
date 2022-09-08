be8f926852bcd7e70cac541c9c1d33182c6360d4
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2022-09-08 18:48:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 def function(argument):
   return '{} function.' .format(argument)
   
-function('Hi')
+print(function('Hi'))
  
+  
+  
+#----
+def function(argument,name='you'):
+  return '{}, {} function.' .format(argument,name)
+  
+print(function('Hi'))
+ 
+  
+  
+def student_info(*args,*kwargs):
+  print(args)
+  print(kwargs)
+
+  
+student_info('math','art',name='Pavan Kumar',Age=24)
